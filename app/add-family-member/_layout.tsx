aa56b1e985d8a95c95086b03f008c1bb7640efae
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-07-23 20:00:21 +0600

    added some screens

---
+++ app/add-family-member/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddFamilyMemberScreen = () => {
+  return (
+    <View>
+      <Text>AddFamilyMemberScreen</Text>
+    </View>
+  )
+}
+
+export default AddFamilyMemberScreen
---
+++ app/add-ticket/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddTicket = () => {
+  return (
+    <View>
+      <Text>AddTicket</Text>
+    </View>
+  )
+}
+
+export default AddTicket
